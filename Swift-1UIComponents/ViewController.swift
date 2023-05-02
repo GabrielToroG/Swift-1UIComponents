a952ff513b9a52cdaa20132d9eb2e6258b9e9475
Gabriel Toro <118211935+GtoroSMU@users.noreply.github.com>
2023-05-02 00:15:47 -0400

    Initial Commit

---
+++ Swift-1UIComponents/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift-1UIComponents
+//
+//  Created by Gabriel Alonso Toro Guzmán on 02-05-23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
